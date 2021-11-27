a91f003d37744472f8ba644d0f6a05782ee7ff05
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-11-27 12:29:50 -0300

    inicial

---
+++ src/test/java/com/barbara/todo/list/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbara.todo.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
