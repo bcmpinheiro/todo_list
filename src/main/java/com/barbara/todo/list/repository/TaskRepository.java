12e1e336f0bab1a267d9521babc966205399f16d
Barbara Pinheiro <80286625+bcmpinheiro@users.noreply.github.com>
2021-11-29 00:44:53 -0300

    criação de camadas para chamada no postman

--- src/main/java/com/barbara/todo/list/repository/TaskRepository.java
+++ src/main/java/com/barbara/todo/list/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.barbara.todo.list.repository;
 
-public class TaskRepository {
+
+import com.barbara.todo.list.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
--- src/main/java/com/barbara/todo/list/service/TaskService.java
+++ src/main/java/com/barbara/todo/list/service/TaskService.java
@@ -1,4 +1,34 @@
 package com.barbara.todo.list.service;
 
+
+import com.barbara.todo.list.model.Task;
+import com.barbara.todo.list.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public Task createTask (Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> listAllTasks(){
+        return taskRepository.findAll();
+    }
+
+    public ResponseEntity<Task> findTaskById(Long id) {
+        return taskRepository.findById(id)
+                .map(task -> ResponseEntity.ok().body(task))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
